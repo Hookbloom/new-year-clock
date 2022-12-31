b782de302e4cd13124cf430f60aa8f918127f28e
Jochem Bloem <45417884+JochemBloem@users.noreply.github.com>
2022-12-31 21:27:08 +0100

    2021 version

---
+++ clock.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from math import floor
+from time import sleep
+
+def printTime():
+	midnight = datetime(2022, 1, 1)
+	now = datetime.now()
+	print (str(round((midnight - now).total_seconds(), 0)) + " seconden")
+
+def main():
+	while (True):
+		sleep(1)
+		printTime()
+
+if _name_ == "_main_":
+	main()
+    
