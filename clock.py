092a727c470bd5a34d37072388f2f7864ab98c37
Loiiise <loisehaak@gmail.com>
2025-11-25 23:47:04 +0100

    generalised second in year finding function

--- clock.py
+++ clock.py
@@ -14,13 +14,25 @@ def printTimeTo2022( offset ):
 def printTimeTo2023():
     
     
-    printTimeTo2022( amountOfSecondsIn2022 )
+    printTimeTo2022( amountOfSecondsInYear(2022) )
 
 def printTimeTo2026():
-    amountOfSecondsIn2023and2024and2025 = (365 + 366 + 365) * 24 * 60 * 60
+    amountOfSecondsIn2023and2024and2025 = (365 + 365) * 24 * 60 * 60 + amountOfSecondsInYear(2024)
     amountOfSecondsBetweenStartOf2022AndEndOf2025 = amountOfSecondsIn2022 + amountOfSecondsIn2023and2024and2025	
     
     printTimeTo2022( amountOfSecondsBetweenStartOf2022AndEndOf2025 )
+
+def amountOfSecondsInYear( year ):
+    match year:
+        case 2022:
+            return amountOfSecondsIn2022
+        case 2024:
+            # 2024 happens to have the same amount of seconds as 2022
+            # except it's a leap year, so we add one extra day!!
+            oneDay = 24 * 60 * 60
+            return amountOfSecondsIn2022 + oneDay 
+        case _:
+            raise "this year is not supported!!"
 
 def main():
 	while (True):
