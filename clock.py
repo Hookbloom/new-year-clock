ed17f5f5719cc59ce7970ac03bf584f50c615c28
Loiiise <loisehaak@gmail.com>
2025-11-25 23:29:39 +0100

    generalised seconds in 2022 for more efficient use

--- clock.py
+++ clock.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from math import floor
 from time import sleep
 
+amountOfSecondsIn2022 = 365 * 24 * 60 * 60
+
 def printTimeTo2022( offset ):
 	midnight = datetime(2022, 1, 1)
 	now = datetime.now()
@@ -10,12 +12,11 @@ def printTimeTo2022( offset ):
 	print (amountOfSecondsLeftStringifiedWithoutDecimal + " seconden")
  
 def printTimeTo2023():
-    amountOfSecondsIn2022 = 365 * 24 * 60 * 60
+    
     
     printTimeTo2022( amountOfSecondsIn2022 )
 
 def printTimeTo2026():
-    amountOfSecondsIn2022 = 365 * 24 * 60 * 60 
     amountOfSecondsIn2023and2024and2025 = (365 + 366 + 365) * 24 * 60 * 60
     amountOfSecondsBetweenStartOf2022AndEndOf2025 = amountOfSecondsIn2022 + amountOfSecondsIn2023and2024and2025	
     
